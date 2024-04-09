71c953a91923fa7688d63ae2e8ab294036ab9634
Alejandro Ramírez <159305481+alejandroramirezvallejos@users.noreply.github.com>
2024-04-09 15:39:07 -0400

    PRUEBA 3/EJERCICIO 18_19

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -313,25 +313,33 @@ void exercise_17(int n) {
 void exercise_18_19(int debut, int fin) {
     int n, k, s, a;
     string d, c;
-    for (int n=debut; n<=fin; n++){
-        int k = 0;
-        int s = n;
-        while (s>0){
-            k++;
-            if (s%3 == 0){
-                s = s + 4;
-            }
-            else if (s%4 == 0){
-                s = s/2;
-            }
-            else{
-                s = s-1;
-            }
-        }
-        c = to_string(n) + " -> " + to_string(k) + " ";
-        d = d + c;
+    if (debut<0){
+        cout<<"El numero debe de ser positivo y mayor a zero"<<endl;
     }
-    a = d.size()-1;
-    d.resize(a);
-    cout<<d<<endl;
+    if (fin<debut){
+        cout<<"El numero de fin no debe de ser menor al inicial"<<endl;
+    }
+    if (debut>=0 && fin>=debut){
+        for (int n=debut; n<=fin; n++){
+            int k = 0;
+            int s = n;
+            while (s>0){
+                k++;
+                if (s%3 == 0){
+                    s = s + 4;
+                }
+                else if (s%4 == 0){
+                    s = s/2;
+                }
+                else{
+                    s = s-1;
+                }
+            }
+            c = to_string(n) + " -> " + to_string(k) + "\n";
+            d = d + c;
+        }
+        a = d.size()-1;
+        d.resize(a);
+        cout<<d<<endl;
+    }    
 }
